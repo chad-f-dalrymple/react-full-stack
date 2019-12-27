b6a4402531da55da07d834e6892f9c6964432c94
Chad Dalrymple <32802734+chad-f-dalrymple@users.noreply.github.com>
2019-12-27 08:18:27 -0500

    upload client side code

---
+++ client/build/precache-manifest.91d4bc61237cbe44119c100a790f9437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060553ef371d3312cb941fa961842ceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fae86b1090a6511301b",
+    "url": "/static/css/2.cd064b02.chunk.css"
+  },
+  {
+    "revision": "0fae86b1090a6511301b",
+    "url": "/static/js/2.80241610.chunk.js"
+  },
+  {
+    "revision": "3fb340deb365552a6635",
+    "url": "/static/js/main.783b66c9.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
